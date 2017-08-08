61197700ba0f1ca459d67dc4e06c74544de840ee
Brad Chen <brad@bradchen.com>
2017-08-07 21:43:13 -0400

    Add a second app.component.ts.

--- src/app/new-test.spec.ts
+++ src/app/new-test.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import * as foo from './foo/app.component';
 
 describe('New Test', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        foo.AppComponent
       ],
     }).compileComponents();
   }));
@@ -28,5 +30,12 @@ describe('New Test', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+  }));
+
+  it('should create app 2', async(() => {
+    const fixture = TestBed.createComponent(foo.AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.doWork();
+    expect(app.title).toBe('foo');
   }));
 });
